793126206b77e0c362898d3aa2b1a47c1ac2b303
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-04-27 13:41:45 +0200

    Initial commit

---
+++ main.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Purpose: Add a confirmation when we are deleting important files
+#          Otherwise delete file without confirmation
+#	   Author: Guillaume MORET
+# --------------------------------------"
+
+
+
