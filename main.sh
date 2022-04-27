ddb6580eb32d5c415806cfd8a27a57cf25a0c789
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-04-27 16:17:32 +0200

    Finishing a script of verification of the importance of the file by adding a confirmation if we want to delete it

--- main.sh
+++ main.sh
@@ -3,6 +3,24 @@
 #          Otherwise delete file without confirmation
 #	   Author: Guillaume MORET
 # --------------------------------------"
-
-
-
+cd $1
+VAR="`ls -a | grep .important `"
+echo $VAR
+answer=""
+if [ $VAR ]; then
+	while [[ $answer != y || $answer != n ]];do    
+	echo "This file contains a file called .important. Do you want to delete it? (y/n)"
+   	 read answer 
+    if [ $answer = "y" ]; then
+	rm -r $1
+        echo "It was important and you deleted it"
+	break
+    elif [ $answer = "n" ]; then
+       echo "You didn't delete it"
+	break
+    fi
+    done
+else
+	rm -r $1
+   	echo "It contained no important file and you deleted it"	
+fi
